bc9aae351985f0a65e284be355a993c4a60238b4
Mauro Silberberg <maurosilber@gmail.com>
2019-11-21 11:33:09 -0300

    Updated setup.py to include tabulate package as requirement.
    Signed-off-by: Mauro Silberberg <maurosilber@gmail.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Mauro Silberberg',
     author_email='maurosilber@gmail.com',
     description='A monkey-patching of Luigi.',
-    install_requires=['luigi', 'click'],
+    install_requires=['luigi', 'click', 'tabulate'],
     extras_requires=['numpy', 'pandas', 'tifffile'],
     entry_points={'console_scripts': ['donkey-kong = donkey_kong.scripts.main:main']}
 )
