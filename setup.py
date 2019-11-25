63b17ec6a8077ab85a050e0b4cc4a8357db3efb7
Mauro Silberberg <maurosilber@gmail.com>
2019-11-24 22:36:53 -0300

    Renamed package from donkey-kong to donkeykong
    donkey-kong already existed in PyPI.
    
    Signed-off-by: Mauro Silberberg <maurosilber@gmail.com>

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='donkey-kong',
+    name='donkeykong',
     version='0.0.0',
     packages=find_packages(),
-    url='https://github.com/maurosilber/donkey-kong',
+    url='https://github.com/maurosilber/donkeykong',
     license='MIT',
     author='Mauro Silberberg',
     author_email='maurosilber@gmail.com',
     description='A monkey-patching of Luigi.',
     install_requires=['luigi', 'click', 'tabulate'],
     extras_requires=['numpy', 'pandas', 'tifffile'],
-    entry_points={'console_scripts': ['donkey-kong = donkey_kong.scripts.main:main']}
+    entry_points={'console_scripts': ['donkeykong = donkeykong.scripts.main:main']}
 )
